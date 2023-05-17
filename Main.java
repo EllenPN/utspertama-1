5f4c3259ac77b1e88902c2f6de789628c5ba392f
EllenPN <ellenputri20@gmail.com>
2023-05-17 22:07:54 +0700

    add to contact page

--- Main.java
+++ Main.java
@@ -1,5 +1,17 @@
 class Main {
     public static void main(String[] args) {
+        
+        Mhs mhs = new(username:"ini_username", password:"ini_password");
+
+        mhs.setNama(nama:"Ellen Putri N");
+        mhs.setNim(nim:"G.111.19.0014");
+        mhs.setSmt(smt:8);
+
+        System.out.println("Nama Mhs:" + mhs.getNama());
+        System.out.println("Nama Mhs:" + mhs.getNim());
+        System.out.println("Nama Mhs:" + mhs.getSmt());
+        System.out.println("Nama Mhs:" + mhs.getUsername());
+        System.out.println("Nama Mhs:" + mhs.getPassword());
 
     }
 }
--- pengguna/User.java
+++ pengguna/User.java
@@ -1,5 +1,34 @@
 package pengguna;
 
 public class User {
+    private String nama ;
+    private String nim ;
+    private int smt ;
+    private boolean password ;
+    private String username ;
+
+    public void setNama(String nama){
+        this.nama = nama;
+    }
+
+public String getNama(){
+    return this.nama;
+}
+
+    public void setNim(String nim){
+        this.nim = nim;
+    }
+
+public String getNim(){
+    return this.nim;
+}
+
+    public void setSmt(int smt){
+        this.smt = smt;
+    }
+
+public String (getSmt){
+    return this.smt;
+}
 
 }
